1f52829b6f802e7cb07fc527bfdd6cf992518803
Daan <daan.passchier@xs4all.nl>
2020-10-15 15:29:03 +0200

    Inladen van glb object werkt

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 import * as THREE from './three.module.js';
+import { GLTFLoader } from './GLTFLoader.js'; 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const loader = new GLTFLoader();
 
-const renderer = new THREE.WebGLRenderer();
+loader.load( 'objects/dummy.glb', ( gltf ) => {
+    const root = gltf.scene;
+    scene.add( root );
+    render();
+
+}, (xhr) => {
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+}, ( error ) => {
+    console.log(error);
+    
+} );
+
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
 
-const animate = () => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    requestAnimationFrame(animate);
+const render = () => {
     renderer.render(scene, camera);
 };
-
-animate();
