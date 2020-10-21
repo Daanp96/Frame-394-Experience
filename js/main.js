b33da5b742119a8bfc17cb332676a95beca32f49
Daan <daan.passchier@xs4all.nl>
2020-10-21 17:39:45 +0200

    Comments toegevoegd aan main.js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,51 @@ import { GLTFLoader } from './GLTFLoader.js';
 import { OrbitControls } from './OrbitControls.js'; 
 
 const main = () => {
+    // Hier pak ik de canvas waar ik de 3D objecten op wil gaan renderen.
     const canvas = document.querySelector('#c');
+
+    // Hier roep ik de renderer aan, die ervooor zorgt dat je alle
+    // 3D objecten op je webpagina kunt renderen.
     const renderer = new THREE.WebGLRenderer({canvas});
     
+    // Hier geef ik alle specificaties mee aan de camera, zoals de "field of view",
+    // en vanaf welke plekken er gerendered moet worden.
     const fov = 75;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
     const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+    // Hier geef ik de positie aan voor de camera.
     camera.position.z = 15;
     camera.position.y = 3;
 
+    // Hier maak ik de controls aan.
     const controls = new OrbitControls(camera, canvas);
-    controls.target.set(0, 5, 0);
+    controls.target.set(0, 2, 0);
     controls.update();
 
+    // Hier maak ik de "Scene" aan, de plek waar alles samenkomt en wordt doorgegeven wat gerendered
+    // moet worden door de WebGL API.
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('red');
 
+    {
+        // Hier maak ik het licht aan.
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+
+    // Hier maak ik het grid aan.
     const size = 10;
     const divisions =  10;
     const gridHelper = new THREE.GridHelper(size, divisions);
     scene.add(gridHelper);
 
+    // Hier roep ik de Loader aan, die ervoor zorgt dat ik zelfgemaakte objecten kan inladen.
     const loader = new GLTFLoader();
     loader.load( 'objects/dummy.glb', ( gltf ) => {
         const root = gltf.scene;
@@ -37,6 +59,7 @@ const main = () => {
         console.log(error);
     } );
 
+    // Een functie die ervoor zorgt dat het canvas responsive is.
     const resizeRendererToDisplaySize = (renderer) => {
         const canvas = renderer.domElement;
         const width = canvas.clientWidth;
@@ -50,6 +73,8 @@ const main = () => {
         return needResize;
     };
 
+    // Een aparte render functie die ervoor zorgt dat alles naar het canvas 
+    // gerendered wordt.
     const render = () => {
         if (resizeRendererToDisplaySize(renderer)) {
             const canvas = renderer.domElement;
