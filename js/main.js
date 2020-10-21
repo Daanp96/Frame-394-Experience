dc5f5c387be044923c3e6216ee67fcb3410f0905
Daan <daan.passchier@xs4all.nl>
2020-10-21 12:50:22 +0200

    Controls en grid toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,33 +1,67 @@
 import * as THREE from './three.module.js';
-import { GLTFLoader } from './GLTFLoader.js'; 
+import { GLTFLoader } from './GLTFLoader.js';
+import { OrbitControls } from './OrbitControls.js'; 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const loader = new GLTFLoader();
-
-loader.load( 'objects/dummy.glb', ( gltf ) => {
-    const root = gltf.scene;
-    scene.add( root );
-    render();
-
-}, (xhr) => {
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-}, ( error ) => {
-    console.log(error);
+const main = () => {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
     
-} );
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 15;
+    camera.position.y = 3;
 
-const canvas = document.querySelector('#c');
-const renderer = new THREE.WebGLRenderer({
-    canvas,
-    alpha: true
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
 
-camera.position.z = 5;
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('red');
 
-const render = () => {
-    renderer.render(scene, camera);
+    const size = 10;
+    const divisions =  10;
+    const gridHelper = new THREE.GridHelper(size, divisions);
+    scene.add(gridHelper);
+
+    const loader = new GLTFLoader();
+    loader.load( 'objects/dummy.glb', ( gltf ) => {
+        const root = gltf.scene;
+        scene.add( root );
+        render();
+    }, (xhr) => {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    }, ( error ) => {
+        console.log(error);
+    } );
+
+    const resizeRendererToDisplaySize = (renderer) => {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+
+        if (needResize) {
+          renderer.setSize(width, height, false);
+        }
+
+        return needResize;
+    };
+
+    const render = () => {
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+          }
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    };
+
+    requestAnimationFrame(render);
 };
+
+main();
