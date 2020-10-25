1de4fd640e8c98072c24f05bf67636e40208ecd8
MatsAlkemade <matsalkemade@gmail.com>
2020-10-25 16:39:40 +0100

    scherm kan opstarten

--- js/Computer_Opstarten.js
+++ js/Computer_Opstarten.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+const screen = document.getElementById("js--screen");
+const onOffButton = document.getElementById("js--onOffButton");
+
+function onOff() {
+    if(screen.style.opacity > .5) {
+        screen.style.opacity = "0";
+        onOffButton.style.pointerEvents = "none";
+        setTimeout(function(){onOffButton.style.pointerEvents = "all";},2000);
+    } else {
+        screen.style.opacity = "1";
+        onOffButton.style.pointerEvents = "none";
+        setTimeout(function(){onOffButton.style.pointerEvents = "all";},2000);
+    }
+}
