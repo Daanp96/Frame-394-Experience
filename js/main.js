7942513313d1ac7653f7cff5f18e4607dd0eb00a
Daan <daan.passchier@xs4all.nl>
2020-11-02 09:45:54 +0100

    Styling done, started with js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const reddit_title = document.getElementById("js--title");
+const gif_upload = document.getElementById("js--gif_input");
+const cancel = document.getElementById("js--cancel");
+const post = document.getElementById("js--post");
+
+gif_upload.addEventListener("click", (e) => {
+    
+});
