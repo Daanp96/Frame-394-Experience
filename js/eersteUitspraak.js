82d81cb5a5db61a95258ced1ee3bb8bb9e86097f
MatsAlkemade <matsalkemade@gmail.com>
2020-12-01 13:32:03 +0100

    labels weggehaald

--- js/eersteUitspraak.js
+++ js/eersteUitspraak.js
@@ -1,42 +1,33 @@
 const checkbox1 = document.getElementById("js--checkbox-1");
 const checkbox2 = document.getElementById("js--checkbox-2");
 const checkbox3 = document.getElementById("js--checkbox-3");
+const choice1 = document.getElementById("js--choice-1")
+const choice2 = document.getElementById("js--choice-2")
+const choice3 = document.getElementById("js--choice-3")
 const continueButton = document.getElementById("js--continueButton");
 
-checkbox1.addEventListener('change', function(){
+choice1.addEventListener('click', function(){
+    checkbox1.checked = true;
     checkbox2.checked = false;
     checkbox3.checked = false;
-    if (checkbox1.checked == false && checkbox2.checked == false && checkbox3.checked == false) {
-        continueButton.style.opacity = "0";
-        continueButton.style.pointerEvents = "none";
-    } else {
-        continueButton.style.opacity = "1";
-        continueButton.style.pointerEvents = "all";
-    }
+    continueButton.style.opacity = "1";
+    continueButton.style.pointerEvents = "all";
 });
 
-checkbox2.addEventListener('change', function(){
+choice2.addEventListener('click', function(){
+    checkbox2.checked = true;
     checkbox1.checked = false;
     checkbox3.checked = false;
-    if (checkbox1.checked == false && checkbox2.checked == false && checkbox3.checked == false) {
-        continueButton.style.opacity = "0";
-        continueButton.style.pointerEvents = "none";
-    } else {
-        continueButton.style.opacity = "1";
-        continueButton.style.pointerEvents = "all";
-    }
+    continueButton.style.opacity = "1";
+    continueButton.style.pointerEvents = "all";
 });
 
-checkbox3.addEventListener('change', function(){
+choice3.addEventListener('click', function(){
+    checkbox3.checked = true;
     checkbox1.checked = false;
     checkbox2.checked = false;
-    if (checkbox1.checked == false && checkbox2.checked == false && checkbox3.checked == false) {
-        continueButton.style.opacity = "0";
-        continueButton.style.pointerEvents = "none";
-    } else {
-        continueButton.style.opacity = "1";
-        continueButton.style.pointerEvents = "all";
-    }
+    continueButton.style.opacity = "1";
+    continueButton.style.pointerEvents = "all";
 });
 
 const overlay = document.getElementById("js--overlay");
@@ -44,6 +35,9 @@ const overlay = document.getElementById("js--overlay");
 window.onload = function() {
     overlay.style.opacity = "0";
     setTimeout(function(){overlay.style.zIndex = "-10";}, 1500);
+    checkbox1.checked = false;
+    checkbox2.checked = false;
+    checkbox3.checked = false;
 }
 
 function nextPage() {
