fc093a327f8121216caaabc0c498724ef80ce70d
MatsAlkemade <matsalkemade@gmail.com>
2020-12-01 16:11:31 +0100

    pop ups toegevoegd en aangepast waar nodig

--- public/js/videoPlayer.js
+++ public/js/videoPlayer.js
@@ -1,7 +1,7 @@
 const viewDiscr = document.getElementById("js--viewDiscr");
 const viewDiscrBg = document.getElementById("js--viewDiscr-bg");
 const continueButton = document.getElementById("js--continue");
-const videoStabilizer = document.getElementById("js--videoStabilizer");
+const exitVideo = document.getElementById("js--exitVideo");
 const overlay = document.getElementById("js--overlay");
 
 function playVideo() {
@@ -16,10 +16,19 @@ function playVideo() {
 
 function skipVideo() {
     viewDiscr.style.animation = "slide-out-top 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
-    videoStabilizer.style.display = "flex";
-    videoStabilizer.style.animation = "slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    exitVideo.style.display = "flex";
+    exitVideo.style.animation = "slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
     continueButton.style.display = "none";
 }
+
+function closeExitVideo(){
+    viewDiscrBg.style.opacity = "0";
+    setTimeout(function(){viewDiscrBg.style.display = "none";}, 1000);
+    exitVideo.style.display = "none";
+    setTimeout(function(){
+        continueButton.style.display = "block";
+        continueButton.style.animation = "slide-in-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+    }, 1000);}
 
 function nextPage() {
     overlay.style.zIndex = "10"
