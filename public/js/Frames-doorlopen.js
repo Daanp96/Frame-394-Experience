d55a4607671d447b6cf856cd2e4a21d4894dbd17
MatsAlkemade <matsalkemade@gmail.com>
2020-12-02 15:42:40 +0100

    frames doorlopen nu echt gefixt

--- public/js/Frames-doorlopen.js
+++ public/js/Frames-doorlopen.js
@@ -4,97 +4,122 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const next = document.getElementById("js--next");
   const button = document.getElementById("js--end-button");
   const taser = document.getElementById("js--taser");
+  const taserText = document.getElementById("js--taser-text");
+  const frameNumber = document.getElementById("js--frame-number")
+  const framePic = document.getElementById("js--frame-picture")
+  let frame = 392;
   
-
-
-
   // maak de terugknop klikbaar
   prev.addEventListener("click", function(){
-    showPreviousSlide(1);
+    // showPreviousSlide(1);
+    frame -= 1;
+    if (frame < 392) {
+      frame = 406;
+    }
+    if (frame == 394) {
+      taser.style.display = "block";
+    } else {taser.style.display = "none";}
+    frameNumber.innerHTML = "Frame #" + frame;
+    framePic.setAttribute("src", "./webp/frame-" + frame + ".webp")
+    taserText.style.display = "none";
   });
 
   // maak de doorknop klikbaaar
   next.addEventListener("click", function(){
-    showNextSlides(1);
+    // showNextSlides(1);
+    prev.style.display = "block";
+    frame += 1;
+    if (frame > 406) {
+      frame = 392;
+    }
+    frameNumber.innerHTML = "Frame #" + frame;
+    framePic.setAttribute("src", "./webp/frame-" + frame + ".webp")
+    if (frame == 394) {
+      taser.style.display = "block";
+    } else {taser.style.display = "none";}
+    if (frame == 406) {
+      button.style.display = "flex";
+    }
+    taserText.style.display = "none";
   });
 
   //maakt de taser in frame 394 klikbaar
   taser.addEventListener("click", function(){
-    showBorder();
+    taserText.style.display = "block";
   })
 
-  function showSlides(n) {
-      let slides = document.getElementsByClassName("mySlides");
-      let l = (n - 1);                                      // dit maakt een variable aan die het getal heeft van n -1. Als je de array ziet als een slideshow is dit dus de vorige slide.
-      let m = (n + 1);                                      // dit maakt een variable aan die het getal heeft van n +1. Als je van rechts naar links door de array heen zou gaan, dan zou dit in de array de vorige slide zijn.
-      let z = (slides.length - 1)                           // dit maakt een variabele aan voor de laatste slide in de array. er zijn 14 slides in de array, tellen begint bij 0 dus de index van de veertiende slide is nummer 13.
-      if (n > 13) {                                         
-       button.style.display = "flex";                     // toon knop als je bij de laatste index van de array bent.
-      }
-      if( n < slides.length){                             //zo lang als we in de array zitten
-        slides[n].style.display = 'block';                // toon de foto van de huidige index 
-        slides[l].style.display = "none";                 // haal de foto van index -1  weg
-        slides[m].style.display = "none";                 // haal de foto van index +1 weg
-        slides[z].style.display = "none";                 // haal de laatste foto van de array weg
-      } else{
-        reset();                                           //roep een reset aan en begin weer op het begin
-      }  
-    }
+//   function showSlides(n) {
+//       let slides = document.getElementsByClassName("mySlides");
+//       let l = (n - 1);                                      // dit maakt een variable aan die het getal heeft van n -1. Als je de array ziet als een slideshow is dit dus de vorige slide.
+//       let m = (n + 1);                                      // dit maakt een variable aan die het getal heeft van n +1. Als je van rechts naar links door de array heen zou gaan, dan zou dit in de array de vorige slide zijn.
+//       let z = (slides.length - 1)                           // dit maakt een variabele aan voor de laatste slide in de array. er zijn 14 slides in de array, tellen begint bij 0 dus de index van de veertiende slide is nummer 13.
+//       if (n > 13) {                                         
+//        button.style.display = "flex";                     // toon knop als je bij de laatste index van de array bent.
+//       }
+//       if( n < slides.length){                             //zo lang als we in de array zitten
+//         slides[n].style.display = 'block';                // toon de foto van de huidige index 
+//         slides[l].style.display = "none";                 // haal de foto van index -1  weg
+//         slides[m].style.display = "none";                 // haal de foto van index +1 weg
+//         slides[z].style.display = "none";                 // haal de laatste foto van de array weg
+//       } else{
+//         reset();                                           //roep een reset aan en begin weer op het begin
+//       }  
+//     }
 
   
 
   
-  let slideIndex = 0;                             
-  showSlides(slideIndex);                         //begin met de functie van het tonen van slides en begin daarmee op de eerste slide.
+//   let slideIndex = 0;                             
+//   showSlides(slideIndex);                         //begin met de functie van het tonen van slides en begin daarmee op de eerste slide.
 
   
-// De vorigeknop zegt toon dia(1 terug)
-  function showPreviousSlide(n){
-    slideIndex = slideIndex - n;
-    showSlidesReversed(slideIndex);
-  }
+// // De vorigeknop zegt toon dia(1 terug)
+//   function showPreviousSlide(n){
+//     slideIndex = slideIndex - n;
+//     showSlidesReversed(slideIndex);
+//   }
 
-  // De volgendeknop zegt toon dia(1 verder dan nu)
-  function showNextSlides(n) {
-    slideIndex = slideIndex + n;
-    showSlides(slideIndex);
-  }
+//   // De volgendeknop zegt toon dia(1 verder dan nu)
+//   function showNextSlides(n) {
+//     slideIndex = slideIndex + n;
+//     showSlides(slideIndex);
+//   }
 
-  function showSlidesReversed(n){
-    let slides = document.getElementsByClassName("mySlides");               
-    let m = (n + 1);
+//   function showSlidesReversed(n){
+//     let slides = document.getElementsByClassName("mySlides");               
+//     let m = (n + 1);
 
-    if( n < 0){
-      reset2();               //roep een reset aan en begin op de laatste slide.
-    } else{
-      slides[n].style.display = 'block';            //toon de huidige slide
-      slides[m].style.display = "none";             // stop met het tonen van slide +1
-    }    
-  }
+//     if( n < 0){
+//       reset2();               //roep een reset aan en begin op de laatste slide.
+//     } else{
+//       slides[n].style.display = 'block';            //toon de huidige slide
+//       slides[m].style.display = "none";             // stop met het tonen van slide +1
+//     }    
+//   }
 
-  function reset(){
-    slideIndex = 0;                           // variabele met het getal voor de index van de eerste slide
-    let slides = document.getElementsByClassName("mySlides");
-    let z = (slides.length - 1)             //variabele voor het getal voor de index van de laatste slide.
-    slides[z].style.display = 'none';       // hou  op met tonen foto laatste slide
-    showSlides(slideIndex);                 //begin met tonen slides bij de allereerste slide.
-  }
+//   function reset(){
+//     slideIndex = 0;                           // variabele met het getal voor de index van de eerste slide
+//     let slides = document.getElementsByClassName("mySlides");
+//     let z = (slides.length - 1)             //variabele voor het getal voor de index van de laatste slide.
+//     slides[z].style.display = 'none';       // hou  op met tonen foto laatste slide
+//     showSlides(slideIndex);                 //begin met tonen slides bij de allereerste slide.
+//   }
 
-  function reset2(){
-    let slides = document.getElementsByClassName("mySlides");
-    let z = 0;                                //variabele voor het getal van de index van de eerste slide
-    slides[z].style.display = 'none';         // hou met tonen eerste slide.
-    slideIndex = (slides.length -1);        // variabele voor de index van de laatste slide.
-    showSlidesReversed(slideIndex);       // begin met het tonen van slides en begin met de allerlaatste.
-  }
+//   function reset2(){
+//     let slides = document.getElementsByClassName("mySlides");
+//     let z = 0;                                //variabele voor het getal van de index van de eerste slide
+//     slides[z].style.display = 'none';         // hou met tonen eerste slide.
+//     slideIndex = (slides.length -1);        // variabele voor de index van de laatste slide.
+//     showSlidesReversed(slideIndex);       // begin met het tonen van slides en begin met de allerlaatste.
+//   }
 
-  function showBorder(){
-    let higlight = document.getElementsByClassName("clickable-object");
-    for (let i = 0; i < higlight.length; i++) {
-      const square =   higlight[i];
-      square.style.opacity = "1";                   //stop de onzichtbaarheid van de highlight van de taser.
-    }
-  }
+//   function showBorder(){
+//     let higlight = document.getElementsByClassName("clickable-object");
+//     for (let i = 0; i < higlight.length; i++) {
+//       const square =   higlight[i];
+//       square.style.opacity = "1";                   //stop de onzichtbaarheid van de highlight van de taser.
+//     }
+//   }
 });
 
 const overlay = document.getElementById("js--overlay");
